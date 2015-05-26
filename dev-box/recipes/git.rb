9fa4700b4229c78d57657e86e5d7bd3ca6de34bc
Jakob Hilden <jakobhilden@gmail.com>
2015-05-27 00:34:17 +0200

    rename + global gitignore

--- dev-box/recipes/git.rb
+++ dev-box/recipes/git.rb
@@ -1,7 +1,13 @@
 include_recipe 'git'
 
-cookbook_file "#{ENV['HOME']}/.gitconfig" do
-  owner 'jk'
+cookbook_file "/home/#{node['dev-box']['user']}/.gitconfig" do
+  owner node['dev-box']['user']
   action :create
   source ".gitconfig"
+end
+
+cookbook_file "/home/#{node['dev-box']['user']}/.gitignore_global" do
+  owner node['dev-box']['user']
+  action :create
+  source ".gitignore_global"
 end
