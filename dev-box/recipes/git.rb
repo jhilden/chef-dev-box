37859ad128fbc47a5801a50b18cf36e80eecc684
Jakob Hilden <jakobhilden@gmail.com>
2016-01-10 22:18:58 -0500

    install hub CLI tool

--- dev-box/recipes/git.rb
+++ dev-box/recipes/git.rb
@@ -11,3 +11,23 @@ cookbook_file "/home/#{node['dev-box']['user']}/.gitignore_global" do
   action :create
   source ".gitignore_global"
 end
+
+# install "hub"
+
+hub_file_name = "hub-linux-amd64-#{node['dev-box']['git']['hub']['version']}"
+local_hub_tar_file = "/tmp/#{hub_file_name}.tgz"
+
+remote_file local_hub_tar_file do
+  source "https://github.com/github/hub/releases/download/v#{node['dev-box']['git']['hub']['version']}/#{hub_file_name}.tgz"
+end
+
+execute 'extract_hub' do
+  command "tar xzvf #{local_hub_tar_file}"
+  cwd '/usr/local/src'
+  not_if { File.exists? "/usr/local/src/#{hub_file_name}" }
+end
+
+execute 'install_hub' do
+  command "./install"
+  cwd "/usr/local/src/#{hub_file_name}"
+end
