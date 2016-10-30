25d83340c60d36baf4ba9e64be042f5ad28dbe56
Jakob Hilden <jakobhilden@gmail.com>
2016-10-30 18:00:57 +0100

    slack

---
+++ dev-box/recipes/slack.rb
@@ -0,0 +1,10 @@
+
+remote_file "/tmp/slack-desktop.deb" do
+  source "https://downloads.slack-edge.com/linux_releases/slack-desktop-2.2.1-#{arch}.deb"
+  mode 0644
+end
+
+dpkg_package "slack-desktop" do
+  source "/tmp/slack-desktop.deb"
+  action :install
+end
